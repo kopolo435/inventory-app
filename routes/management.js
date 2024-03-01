0ca4c9bf166d32bee6e88d70c2b2f0befbd5a633
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-01 16:37:10 -0500

    Agrega rutas de management

---
+++ routes/management.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.router();
+
+//Require controllers
+const pedido_controller = require("../controllers/pedidoController");
+const user_controller = require("../controllers/userController");
+
+//Pedidos routes
+
+//GET list of pedidos
+router.get("/orders", pedido_controller.pedido_list);
+
+//GET request to create a pedido
+router.get("/order/create", pedido_controller.pedido_create_get);
+
+//POST request to create pedido
+router.post("/order/create", pedido_controller.pedido_create_post);
+
+//GET request to update a pedido
+router.get("/order/:id/update", pedido_controller.pedido_update_get);
+
+//POST request to update a pedido
+router.post("/order/:id/update", pedido_controller.pedido_update_post);
+
+//GET request to delete a pedido
+router.get("/order/:id/delete", pedido_controller.pedido_delete_get);
+
+//POST request to delete a pedido
+router.post("/order/:id/delete", pedido_controller.pedido_delete_post);
+
+//GET request to show pedido details
+router.get("/order/:id");
+
+//User routes
+
+//GET list of all users
+router.get("/users", user_controller.user_list);
+
+//GET request to create a user
+router.get("/users", user_controller.user_create_get);
+
+//POST request to create a user
+router.post("/users", user_controller.user_create_post);
+
+//GET request to update a user
+router.get("/user/:id/update", user_controller.user_update_get);
+
+//POST request to update a user
+router.post("/user/:id/update", user_controller.user_update_post);
+
+//GET request to delete a user
+router.get("user/:id/delete", user_controller.user_update_get);
+
+//POST request to delete a user
+router.post("user/:id/delete", user_controller.user_update_post);
+
+//GET request to show user details
+router.get("user/:id");
