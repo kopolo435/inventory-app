f9d4ef76b2ee39a89ccc6ea7242c02e033eb1a99
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-03 11:49:00 -0500

    Modifica modelo de pedido, agrega fecha de creacion

--- models/pedido.js
+++ models/pedido.js
@@ -5,10 +5,11 @@ const pedidoSchema = new Schema({
   product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   ammount: { type: Number, min: 1, required: true },
+  orderPlaced: { type: Date, default: Date.now },
 });
 
 pedidoSchema.virtual("url").get(function () {
-  return `/pedidos/${this._id}`;
+  return `/management/pedidos/${this._id}`;
 });
 
 module.exports = mongoose.model("Pedido", pedidoSchema);
