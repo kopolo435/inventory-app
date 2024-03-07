9637b47dcdeeb4629f89798f293470eeb3025c9c
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-07 16:13:13 -0500

    Añade link para agregar producto y modifica form

--- models/product.js
+++ models/product.js
@@ -7,6 +7,7 @@ const productSchema = new Schema({
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   price: { type: Number, min: 0, required: true },
   stock: { type: Number, min: 0, default: 0 },
+  img: { type: String },
 });
 
 productSchema.virtual("url").get(function () {
