3dc68b7f7c6e7d3d4818261b09e5562b8ad2e42f
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-08 08:51:42 -0500

    Agrega configuraciones para deploy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+const compression = require("compression");
+const helmet = require("helmet");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -10,16 +12,39 @@ const catalogRouter = require("./routes/catalog");
 const managementRouter = require("./routes/management");
 var app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 //DB connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  "mongodb+srv://samirfanilla:sanjudas435@myatlasclusteredu.pagxys9.mongodb.net/inventory_app_dev?retryWrites=true&w=majority&appName=myAtlasClusterEDU";
+
+const mongoDB = process.env.MONGODB_URI || process.env.DEV_DB_URL;
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
+//Compression of requests
+
+app.use(compression()); // Compress all routes
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
